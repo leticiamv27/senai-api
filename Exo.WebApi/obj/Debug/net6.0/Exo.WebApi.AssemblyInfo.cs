bff5605b2d9e60c3a5f8314296b769454cc157b8
Leticia Vasconcelos <leticiamv@gmail.com>
2024-05-31 21:31:45 -0300

    ultima atividade

--- Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a376e9a0c33cee6bede44de9135c7dc2dc1ab85e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e0d2b5fad21b7cf898a9cf995ed61a4838cd42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
